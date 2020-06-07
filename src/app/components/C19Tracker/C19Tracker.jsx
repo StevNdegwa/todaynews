5c4b88da7408add5a2f30668709b68a3ff015791
Stephen <sndegwa.n@outlook.com>
2020-06-07 16:05:28 +0300

    feature(covid19tracker):create webpage

--- src/app/components/C19Tracker/C19Tracker.jsx
+++ src/app/components/C19Tracker/C19Tracker.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 
+import {format} from "d3-format";
+
 import Header from "../Header";
 import Globe from "./Globe.jsx";
 
-import {fetchSummary, fetchCountries} from "../../lib/news/fetch-c19";
+import {fetchSummary} from "../../lib/news/fetch-c19";
 
 import {Main, Section, Title, Content, Stat, CountriesStats, CSelect, CLoader} from "./styles";
 
@@ -13,6 +15,8 @@ export default function C19Tracker(){
   const [countryData, setCountryData] = React.useState({});
   const [loading, setLoading] = React.useState(false);
   const [error, setError] = React.useState({error:false, message:""});
+  
+  const numsFormat = format(",");
   
   React.useEffect(()=>{
     loadData()
@@ -27,13 +31,10 @@ export default function C19Tracker(){
     setLoading(true);
     try{
       const s = await fetchSummary();
-      
       if(!s.Countries){
         throw new Error("Connection Error")
       }
-      
       setSummary(s);
-      
       findCountryData(s.Countries);
       setLoading(false);
     }catch(error){
@@ -60,27 +61,27 @@ export default function C19Tracker(){
         <Content>
           <Stat>
             <h5>New Confirmed</h5>
-            <div>{summary.Global && summary.Global.NewConfirmed}</div>
+            <div>{summary.Global && numsFormat(summary.Global.NewConfirmed)}</div>
           </Stat>
           <Stat>
             <h5>Total Confirmed</h5>
-            <div>{summary.Global && summary.Global.TotalConfirmed}</div>
+            <div>{summary.Global && numsFormat(summary.Global.TotalConfirmed)}</div>
           </Stat>
           <Stat>
             <h5>New Deaths</h5>
-            <div>{summary.Global && summary.Global.NewDeaths}</div>
+            <div>{summary.Global && numsFormat(summary.Global.NewDeaths)}</div>
           </Stat>
           <Stat>
             <h5>Total Deaths</h5>
-            <div>{summary.Global && summary.Global.TotalDeaths}</div>
+            <div>{summary.Global && numsFormat(summary.Global.TotalDeaths)}</div>
           </Stat>
           <Stat>
             <h5>New Recovered</h5>
-            <div>{summary.Global && summary.Global.NewRecovered}</div>
+            <div>{summary.Global && numsFormat(summary.Global.NewRecovered)}</div>
           </Stat>
           <Stat>
             <h5>Total Recovered</h5>
-            <div>{summary.Global && summary.Global.TotalRecovered}</div>
+            <div>{summary.Global && numsFormat(summary.Global.TotalRecovered)}</div>
           </Stat>
         </Content>
       </Section>
@@ -95,12 +96,12 @@ export default function C19Tracker(){
                 })}
               </CSelect>
             </li>
-            <li><b>New Confirmed:</b> {countryData.NewConfirmed}</li>
-            <li><b>Total Confirmed:</b> {countryData.TotalConfirmed}</li>
-            <li><b>New Deaths:</b> {countryData.NewDeaths}</li>
-            <li><b>Total Deaths:</b> {countryData.TotalDeaths}</li>
-            <li><b>New Recovered:</b> {countryData.NewRecovered}</li>
-            <li><b>Total Recovered:</b> {countryData.TotalRecovered}</li>
+            <li><b>New Confirmed:</b> {countryData.NewConfirmed && numsFormat(countryData.NewConfirmed)}</li>
+            <li><b>Total Confirmed:</b> {countryData.TotalConfirmed && numsFormat(countryData.TotalConfirmed)}</li>
+            <li><b>New Deaths:</b> {countryData.NewDeaths && numsFormat(countryData.NewDeaths)}</li>
+            <li><b>Total Deaths:</b> {countryData.TotalDeaths && numsFormat(countryData.TotalDeaths)}</li>
+            <li><b>New Recovered:</b> {countryData.NewRecovered && numsFormat(countryData.NewRecovered)}</li>
+            <li><b>Total Recovered:</b> {countryData.TotalRecovered && numsFormat(countryData.TotalRecovered)}</li>
           </CountriesStats>
           <Globe country={currCountry}/>
           <div></div>
--- src/app/components/C19Tracker/Globe.jsx
+++ src/app/components/C19Tracker/Globe.jsx
@@ -2,16 +2,17 @@ import React from "react";
 
 import map from "../../data/world-map.json";
 
-import * as d3 from "d3";
+import {geoNaturalEarth1, geoPath, geoGraticule} from "d3-geo";
+import {select} from "d3-selection";
 
 export default function Globe({country}){
   const svg = React.useRef();
   
   var chart, update = false;
   const width = 700, height = 400;
-  const projection = d3.geoNaturalEarth1().scale(120).translate([width/2,height/2]),
-    path = d3.geoPath().projection(projection),
-    graticule = d3.geoGraticule().outline();
+  const projection = geoNaturalEarth1().scale(120).translate([width/2,height/2]),
+    path = geoPath().projection(projection),
+    graticule = geoGraticule().outline();
   
   React.useEffect(()=>{
     if(update){
@@ -22,12 +23,12 @@ export default function Globe({country}){
   }, [country])
   
   function createChart(){
-    chart = d3.select(svg.current).attr("width",width).attr("height",height).append("g");
+    chart = select(svg.current).attr("width",width).attr("height",height).append("g");
     chart.selectAll("path").data(map.features).enter().append("path").attr("d",path).attr("stroke","white").attr("stroke-width",0.3)
     .attr("fill",(d,i)=>{
       return d.properties.CountryCode === country ? "red" : "#eeeeee";
     })
-    chart.selectAll("path.gr").data([graticule]).enter().append("path").classed("gr",true).attr("d",path).attr("stroke","black").attr("stroke-width",0.3).attr("fill","none");
+    chart.selectAll("path.gr").data([graticule]).enter().append("path").classed("gr",true).attr("d",path).attr("stroke","#eeeeee").attr("stroke-width",0.3).attr("fill","none");
     update = true;
   }
   
